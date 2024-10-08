db49c8e180d7086fb7ba9dfd9f38ac7967187131
JustFavour <odeyemifavour1@gmail.com>
2024-10-09 00:39:44 +0100

    modularity and error correction

--- src/utils.sol
+++ src/utils.sol
@@ -1,6 +1,43 @@
-// SPDX-License-Identifier: MIT
 
+
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
+
+library TwarStakingLib {
+    
+    // The minimum function to return the smaller of two values
+    function _min(uint256 x, uint256 y) internal pure returns (uint256) {
+        return x <= y ? x : y;
+    }
+
+    // The earned function for calculating rewards
+    function earned(
+        uint256 userBalance, 
+        uint256 rewardPerTokenStored, 
+        uint256 userRewardPerTokenPaid, 
+      
+        uint256 rewards
+    ) internal pure returns (uint256) {
+        return ((userBalance * (rewardPerTokenStored - userRewardPerTokenPaid)) / 1e18) + rewards;
+    }
+
+ // rewardpertoken is 0 when totalAmountStaked  is 0  and when totalAmountStaked is not 0
+    // then  rewardpertoken = RJ =  RJ0 + R/T (J-J0) where J is in seconds
+    // The rewardPerToken function to calculate the reward rate per token
+    function rewardPerToken(
+        uint256 totalAmountStaked, 
+        uint256 rewardPerTokenStored, 
+        uint256 rewardRate, 
+        uint256 lastTimeForReward, 
+        uint256 lastRewardUpdatedTIme
+    ) internal pure returns (uint256) {
+        if (totalAmountStaked == 0) {
+            return rewardPerTokenStored;
+        } else {
+            return rewardPerTokenStored + (rewardRate * (lastTimeForReward - lastRewardUpdatedTIme) * 1e18) / totalAmountStaked;
+        }
+    }
+}
 
 library TwarStakingError {
     error Staking_ZeroAmount();
@@ -10,6 +47,7 @@ library TwarStakingError {
     error NotOwner();
     error StakingIsActive();
     error DurationisZero();
+    error PoolReward_TooMuch();
 }
 
 library TwarStakingEvent {
